f56bb500375ac716af17d13bc432e3325d106f79
Anne-Sylvie Deutsch <annesylviedeutsch@gmail.com>
2018-06-07 19:55:59 -0400

    Adding gif_size argument to insertion file, and implementing sorting algorithm on photo

--- insertion-photo.py
+++ insertion-photo.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import random as rand
 from matplotlib import animation
 import argparse
+import matplotlib.image as mpimg
 from hist import *
 
 
@@ -28,36 +29,34 @@ if __name__ == '__main__':
     
     # Gets argument from command line if specified or set it to default value
     parser = argparse.ArgumentParser()
-    parser.add_argument("--pixel", help="Size (number of pixels) of the matrix to be generated", type=int)
+    parser.add_argument("--picture", help="Path to the picture to be sorted", type=str)
     parser.add_argument("--gif_size", help="Size of the output gif", type=int)
     args = parser.parse_args()
-    if args.pixel:
-        sq_size = args.pixel
-    else:
-        sq_size = 64
     if args.gif_size:
         fig_size = args.gif_size
     else:
         fig_size = 3
+    if args.picture:
+        im_path = args.picture
+    else:
+        raise Exception()
 
-    # Generates square matrix of size sq_size, where each row is a list of numbers from 1 to sq_size that is then shuffled.
-    sorted_row = list(range(sq_size))
-    M = [sorted_row[:] for _ in range(sq_size)]
-    for row in M:
-        rand.shuffle(row)
+    # Load Image
+    img = mpimg.imread(im_path)
 
     # Apply the sorting algorithm on random matrix and get the list of matrix histories
-    M_hist = line2mat_hist(sort_hist_gen(M, insertion_sort))
-
+    #M_hist = line2mat_hist(insertionSortMatrix(img.tolist()))
+    M_hist = line2mat_hist(sort_hist_gen(img.tolist(), insertion_sort))
+    
     # Prepares the imshow plot
     fig = plt.figure(figsize=(fig_size, fig_size))
     ax = plt.Axes(fig, [0., 0., 1., 1.])
     ax.set_axis_off()
     ax.set_aspect(1)
     fig.add_axes(ax)
-    im = plt.imshow(M, interpolation="none")
+    im = plt.imshow(img, interpolation="none")
     plt.axis("off")
 
     # Animates and saves the animation
     anim = animation.FuncAnimation(fig, animate, fargs=(M_hist, im,), frames=len(M_hist), interval=20, blit=True)
-    anim.save(f'insertion{sq_size}.gif', writer='imagemagick', fps=60)
+    anim.save('insertion-photo.gif', writer='imagemagick', fps=60)
